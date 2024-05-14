f0705ee93a65605d0a0bfdb8397bbc3ae364107c
noeliaariza <noelia.ariza@gmail.com>
2024-05-14 15:29:33 +0200

    functional components to class components

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,26 +1,50 @@
 import "../css/Testimonio.css";
 import PropTypes from "prop-types";
+import { Component } from "react";
 
-function Testimonio(props) {
-  return (
-    <div className="contenedor-testimonio">
-      <img
-        className="imagen-testimonio"
-        src={props.img}
-        alt={`Foto de ${props.nombre}`}
-      />
-      <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">
-          <strong>{props.nombre}</strong> en {props.pais}
-        </p>
-        <p className="cargo-testimonio">
-          {props.cargo} en <strong>{props.empresa}</strong>
-        </p>
-        <p className="texto-testimonio">{props.testimonio}</p>
+class Testimonio extends Component {
+  render() {
+    return (
+      <div className="contenedor-testimonio">
+        <img
+          className="imagen-testimonio"
+          src={this.props.img}
+          alt={`Foto de ${this.props.nombre}`}
+        />
+        <div className="contenedor-texto-testimonio">
+          <p className="nombre-testimonio">
+            <strong>{this.props.nombre}</strong> en {this.props.pais}
+          </p>
+          <p className="cargo-testimonio">
+            {this.props.cargo} en <strong>{this.props.empresa}</strong>
+          </p>
+          <p className="texto-testimonio">{this.props.testimonio}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+// function Testimonio(props) {
+//   return (
+//     <div className="contenedor-testimonio">
+//       <img
+//         className="imagen-testimonio"
+//         src={props.img}
+//         alt={`Foto de ${props.nombre}`}
+//       />
+//       <div className="contenedor-texto-testimonio">
+//         <p className="nombre-testimonio">
+//           <strong>{props.nombre}</strong> en {props.pais}
+//         </p>
+//         <p className="cargo-testimonio">
+//           {props.cargo} en <strong>{props.empresa}</strong>
+//         </p>
+//         <p className="texto-testimonio">{props.testimonio}</p>
+//       </div>
+//     </div>
+//   );
+// }
 
 Testimonio.propTypes = {
   img: PropTypes.string,
